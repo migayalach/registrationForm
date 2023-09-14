cc0963ff7cde71de587703675809a7c01fdaa445
Miguel ACH <ayalachavezmiguel@gmail.com>
2023-09-14 10:55:26 -0400

    ruta de usuario

---
+++ server/src/routes/userRoute.js
@@ -0,0 +1,9 @@
+const {Router} = require("express");
+
+const userRouter = Router();
+// userRouter.post();
+// userRouter.get();
+// userRouter.put();
+// userRouter.delete("/:idUser");
+
+module.exports = userRouter;
