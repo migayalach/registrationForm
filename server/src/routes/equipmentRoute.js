473cc8507795c312f3138ee575c23bd1513d84dd
Miguel ACH <ayalachavezmiguel@gmail.com>
2023-09-17 22:56:17 -0400

    preparando para equipo

---
+++ server/src/routes/equipmentRoute.js
@@ -0,0 +1,11 @@
+const {Router} = require("express");
+
+const equipmentRouter = Router();
+
+// equipmentRouter.post("/",);
+// equipmentRouter.get("/",);
+// equipmentRouter.get("/:idEquipment",);
+// equipmentRouter.put("/",);
+// equipmentRouter.delete("/:idEquipment",);
+
+module.exports = equipmentRouter;
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -4,11 +4,13 @@ const mainRouter = Router();
 // route's
 const levelRouter = require("./levelRoute");
 const userRouter = require("./userRoute");
-const categoryRouter = require("./categoryRoute")
+const categoryRouter = require("./categoryRoute");
+const equipmentRouter = require("./equipmentRoute");
 
 // entry points
 mainRouter.use("/level", levelRouter);
 mainRouter.use("/user", userRouter);
-mainRouter.use("/category", categoryRouter)
+mainRouter.use("/category", categoryRouter);
+mainRouter.use("/equipment", equipmentRouter);
 
 module.exports = mainRouter;
