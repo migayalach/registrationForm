71d1d37222e368c84f63b361e88ec3f3effbe9b2
migayalach <ayalachavezmiguel@gmail.com>
2023-09-29 10:29:37 -0400

    tabla credencial

---
+++ client_form/src/Components/TableCredential.jsx
@@ -0,0 +1,21 @@
+const TableCredential = () => {
+  return (
+    <div>
+      <p>3.- Credenciales de acceso: </p>
+      <table border="1">
+        <tr>
+          <td>1.- Asistencia de Control de Asistencia</td>
+          <td>2.- Dominio institucional</td>
+          <td>3.- Sistema de gestion de correspondencia</td>
+        </tr>
+        <tr>
+          <td>4.- Sistema para el pedido de almacenes </td>
+          <td>5.- Correo institucional</td>
+          <td> </td>
+        </tr>
+      </table>
+    </div>
+  );
+};
+
+export default TableCredential;
