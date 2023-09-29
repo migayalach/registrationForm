16d23890d6af4888081367f634e5bbacc3962f71
migayalach <ayalachavezmiguel@gmail.com>
2023-09-29 10:30:00 -0400

    tabla equiṕo

---
+++ client_form/src/Components/TableEquipment.jsx
@@ -0,0 +1,23 @@
+const TableEquipment = () => {
+  return (
+    <div>
+      <p>2.- Equipos de computacion: </p>
+      <table border="1">
+        <tr>
+          <td>Tipo: </td>
+          <td>Fila 1, Columna 2</td>
+          <td>Host: </td>
+          <td>Fila 1, Columna 2</td>
+        </tr>
+        <tr>
+          <td>Direccion IP: </td>
+          <td>Fila 1, Columna 2</td>
+          <td>Etiqueta de control: </td>
+          <td>Fila 1, Columna 2</td>
+        </tr>
+      </table>
+    </div>
+  );
+};
+
+export default TableEquipment;
