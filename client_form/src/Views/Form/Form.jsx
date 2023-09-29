cca0b1f4217661da64d2046d183ca9cde31a4f2f
migayalach <ayalachavezmiguel@gmail.com>
2023-09-29 10:28:38 -0400

    formulario

--- client_form/src/Views/Form/Form.jsx
+++ client_form/src/Views/Form/Form.jsx
@@ -1,8 +1,15 @@
+import TableUserApi from "../../Components/TableUserApi";
+import TableEquipment from "../../Components/TableEquipment";
+import TableCredential from "../../Components/TableCredential";
+
 const Form = () => {
   return (
     <div>
       <h1>Formulario de Alta de Usuarios</h1>
       <h3>FORM-UTIC-001</h3>
+      <TableUserApi />
+      <TableEquipment />
+      <TableCredential />
     </div>
   );
 };
