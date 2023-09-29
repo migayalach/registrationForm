50b2c7855fd63eb2b7c61e1ef95b508d7b87f2eb
migayalach <ayalachavezmiguel@gmail.com>
2023-09-29 10:29:12 -0400

    texto

---
+++ client_form/src/Components/LabelText.jsx
@@ -0,0 +1,35 @@
+const text = {
+  textEquipment: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, ex
+  aspernatur iusto veniam aut error in odio vel quo dolores inventore,
+  tempora, a nesciunt voluptate animi quae totam! Doloremque, omnis! Omnis
+  minima nostrum ut. Eius illo, laudantium, veniam commodi nesciunt
+  dignissimos nisi rem odit voluptas quibusdam soluta suscipit repellat
+  error earum fuga excepturi recusandae exercitationem similique facilis
+  totam qui consectetur. Maxime et incidunt excepturi voluptatum fugiat ad
+  culpa vero ut obcaecati labore modi odit, consequatur quia eveniet sequi
+  voluptates quis ratione suscipit, necessitatibus voluptatem, architecto
+  autem cum soluta. Fugit, eaque. Expedita quaerat nesciunt assumenda
+  fugit, dolorum similique corrupti blanditiis eligendi eum et a alias
+  ratione, voluptate, in commodi? Repudiandae quos earum odit cumque quas
+  aperiam, et esse eos error consectetur! Assumenda dolorum omnis nisi
+  nemo cupiditate soluta ducimus maxime iusto tenetur aliquam, rem fugit
+  asperiores adipisci illo a odio eos non quibusdam ipsam consectetur
+  nulla, unde voluptatum tempore! Pariatur, consectetur.`,
+  textCredential: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+  accusantium, quae iste doloribus unde facere corrupti nobis quasi
+  perspiciatis atque placeat animi. Quas laudantium aspernatur fugit
+  officia sit nobis quis! Reprehenderit necessitatibus, suscipit earum
+  enim deserunt commodi fugit consectetur cumque. Amet fugiat laboriosam
+  ex rem nam cum hic in ipsam doloremque deserunt, impedit magni deleniti
+  explicabo dicta corporis minus asperiores.`,
+};
+
+const LabelText = () => {
+  return (
+    <div>
+      <p>{text}</p>
+    </div>
+  );
+};
+
+export default LabelText;
