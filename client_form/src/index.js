40915c2890a984ab2eb8b7d9a7bbdbe0b654bc8b
migayalach <ayalachavezmiguel@gmail.com>
2023-10-01 15:49:43 -0400

    index.js listo

--- client_form/src/index.js
+++ client_form/src/index.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './StyleSheets/index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./StyleSheets/index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./Redux/store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
