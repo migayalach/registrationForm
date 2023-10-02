0f3aa81b5aa7731ac7be8d240dd0cea406ac8359
migayalach <ayalachavezmiguel@gmail.com>
2023-10-02 10:01:51 -0400

    redux-actions

---
+++ client_form/src/Redux/actions-type.js
@@ -0,0 +1,58 @@
+// STATE
+export const ADD_STATE = "ADD_STATE";
+export const GET_STATE = "GET_STATE";
+export const GET_STATE_ID = "GET_STATE_ID";
+export const UPDATE_STATE = "UPDATE_STATE";
+export const DELETE_STATE = "DELETE_STATE";
+
+// PROCEDURES
+export const ADD_PROCEDURES = "ADD_PROCEDURES";
+export const GET_PROCEDURES = "GET_PROCEDURES";
+export const GET_PROCEDURES_ID = "GET_PROCEDURES_ID";
+export const UPDATE_PROCEDURES = "UPDATE_PROCEDURES";
+export const DELETE_PROCEDURES = "DELETE_PROCEDURES";
+
+// USER API
+export const ADD_USER_API = "ADD_USER_API";
+export const GET_USER_API = "GET_USER_API";
+export const GET_USER_API_ID = "GET_USER_API_ID";
+export const UPDATE_USER_API = "UPDATE_USER_API";
+export const DELETE_USER_API = "DELETE_USER_API";
+
+// EQUIPMENT
+export const ADD_EQUIPMENT = "ADD_EQUIPMENT";
+export const GET_EQUIPMENT = "GET_EQUIPMENT";
+export const GET_EQUIPMENT_ID = "GET_EQUIPMENT_ID";
+export const UPDATE_EQUIPMENT = "UPDATE_EQUIPMENT";
+export const DELETE_EQUIPMENT = "DELETE_EQUIPMENT";
+
+// AREA
+export const ADD_AREA = "ADD_AREA";
+export const GET_AREA = "GET_AREA";
+export const GET_AREA_ID = "GET_AREA_ID";
+export const UPDATE_AREA = "UPDATE_AREA";
+export const DELETE_AREA = "DELETE_AREA";
+
+// USER
+export const ADD_USER = "ADD_USER";
+export const GET_USER = "GET_USER";
+export const GET_USER_ID = "GET_USER_ID";
+export const UPDATE_USER = "UPDATE_USER";
+export const DELETE_USER = "DELETE_USER";
+
+// CREDENTIAL
+export const ADD_CREDENTIAL = "ADD_CREDENTIAL";
+export const GET_CREDENTIAL = "GET_CREDENTIAL";
+export const GET_CREDENTIAL_ID = "GET_CREDENTIAL_ID";
+export const UPDATE_CREDENTIAL = "UPDATE_CREDENTIAL";
+export const DELETE_CREDENTIAL = "DELETE_CREDENTIAL";
+
+// FORM
+export const ADD_FORM = "ADD_FORM";
+export const GET_FORM = "GET_FORM";
+export const GET_FORM_ID = "GET_FORM_ID";
+export const UPDATE_FORM = "UPDATE_FORM";
+export const DELETE_FORM = "DELETE_FORM";
+
+
+// FILTROS
