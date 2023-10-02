093bebb8e3147761b83135082c300e20e711c645
migayalach <ayalachavezmiguel@gmail.com>
2023-10-02 10:12:12 -0400

    redux => reducer

--- client_form/src/Redux/reducer.js
+++ client_form/src/Redux/reducer.js
@@ -1,9 +1,36 @@
-const initialState = {};
+import {
+  ADD_STATE,
+  GET_STATE,
+  GET_STATE_ID,
+  UPDATE_STATE,
+  DELETE_STATE,
+} from "./actions-type";
+
+const initialState = {
+  state: [],
+};
 
 const rootReducer = (state = initialState, { type, payload }) => {
   switch (type) {
-    // case value:
-    //   break;
+    case ADD_STATE:
+      try {
+      } catch (error) {}
+
+    // case GET_STATE:
+    //   try {
+    //   } catch (error) {}
+
+    // case GET_STATE_ID:
+    //   try {
+    //   } catch (error) {}
+
+    // case UPDATE_STATE:
+    //   try {
+    //   } catch (error) {}
+
+    // case DELETE_STATE:
+    //   try {
+    //   } catch (error) {}
 
     default:
       return {
