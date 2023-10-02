f6328cfd666a0a0b5071d1c56456ccf75a1ea66a
migayalach <ayalachavezmiguel@gmail.com>
2023-10-02 10:02:36 -0400

    user

--- client_form/src/Views/User/User.jsx
+++ client_form/src/Views/User/User.jsx
@@ -8,6 +8,9 @@ const User = () => {
   return (
     <>
       <h1>User</h1>
+      <p>aca va el formuilario que se te permite crear usuarios</p>
+      <hr />
+      <p>Aca ira la lista de los usuarios</p>
     </>
   );
 };
