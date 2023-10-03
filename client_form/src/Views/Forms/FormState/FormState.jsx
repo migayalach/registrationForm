65ba241667ec7c6d154708b65ffd6eeb325340de
migayalach <ayalachavezmiguel@gmail.com>
2023-10-02 22:05:00 -0400

    hasta ahora todo va bien

--- client_form/src/Views/Forms/FormState/FormState.jsx
+++ client_form/src/Views/Forms/FormState/FormState.jsx
@@ -1,18 +1,42 @@
 // COMPONET'S
+import { ButtonAccept } from "../../../Components/ButtonAccept";
 
 // HOOK'S
 import { useState } from "react";
 
 // JAVASCRIP
-import validationName from "../../../Validations/validationName";
+import { validationName } from "../../../Validations/validationName";
 
 // STYLESHEET'S
 
 const FormState = () => {
+  const [userData, setUserData] = useState({
+    name: "",
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (event) => {
+    setUserData({
+      ...userData,
+      [event.target.name]: event.target.value,
+    });
+    setErrors(
+      validationName({ ...userData, [event.target.name]: event.target.value })
+    );
+  };
+
   return (
     <form className="form-component">
       <label htmlFor="nombre">Nombre: </label>
-      <input type="text" />
+      <input
+        type="text"
+        value={userData.name}
+        name="name"
+        onChange={handleChange}
+      />
+      {errors.name && <p className="error">{errors.name}</p>}
+      {!errors.name && <ButtonAccept label={"Aceptar"} />}
     </form>
   );
 };
