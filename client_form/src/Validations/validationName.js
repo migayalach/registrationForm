8aa5978f7cf6693dc8e283baaa3519d667f2a0c0
migayalach <ayalachavezmiguel@gmail.com>
2023-10-02 21:24:31 -0400

    formulario validacion name, corregido

--- client_form/src/Validations/validationName.js
+++ client_form/src/Validations/validationName.js
@@ -1,9 +1,14 @@
-export const validationName = (userName) => {
+const validationName = ({ name }) => {
   const errorName = {};
 
-  if (userName.name.length > 35) {
-    errorName.name = `El nombre de usuario no puede tener mas de 35 caracteres`;
+  if (name.length > 35) {
+    errorName.name = `El nombre no puede tener mas de 35 caracteres`;
+  }
+  if (name.length === 0) {
+    errorName.name = `El nombre no puede estar vacio`;
   }
 
   return errorName;
 };
+
+export default validationName;
