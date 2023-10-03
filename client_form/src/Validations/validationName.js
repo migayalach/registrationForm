1998d52a304bdbfe28927bfb85235d05528ecfb8
migayalach <ayalachavezmiguel@gmail.com>
2023-10-02 22:03:23 -0400

    validation name, listo

--- client_form/src/Validations/validationName.js
+++ client_form/src/Validations/validationName.js
@@ -1,14 +1,25 @@
-const validationName = ({ name }) => {
-  const errorName = {};
+import { nameRegex } from "../Utils/regex";
 
-  if (name.length > 35) {
-    errorName.name = `El nombre no puede tener mas de 35 caracteres`;
+export const validationName = ({ name }) => {
+  const errorName = {};
+  const maxLengthName = 20;
+  const minLengthName = 5;
+
+  if (!nameRegex.test(name)) {
+    errorName.name = `La primera letra no puede ser caracter especial ni numero`;
   }
+
   if (name.length === 0) {
     errorName.name = `El nombre no puede estar vacio`;
   }
 
+  if (name.length < minLengthName) {
+    errorName.name = `El nombre no puede tener menos de ${minLengthName} caracteres`;
+  }
+
+  if (name.length > maxLengthName) {
+    errorName.name = `El nombre no puede tener mas de ${maxLengthName} caracteres`;
+  }
+
   return errorName;
 };
-
-export default validationName;
