e5f6a06c0d52c1d19fee8c271f12c2195df3576a
migayalach <ayalachavezmiguel@gmail.com>
2023-10-03 12:26:34 -0400

    implementacion de hooks, statem y effect

--- client_form/src/Views/State/State.jsx
+++ client_form/src/Views/State/State.jsx
@@ -1,11 +1,31 @@
 // COMPONET'S
 import FormState from "../Forms/FormState/FormState";
 import Filter from "../../Components/Filter/Filter";
+import Lists from "../../Components/Lists";
+
 // HOOK'S
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+//REDUX
+import { getAllState } from "../../Redux/actions";
 
 // STYLESHEET'S
+import "./state.css";
 
 const State = () => {
+  const dispatch = useDispatch();
+  const selectorState = useSelector((state) => state.state);
+  const [items, setItems] = useState(selectorState);
+
+  useEffect(() => {
+    dispatch(getAllState);
+  }, [dispatch]);
+
+  useEffect(() => {
+    setItems(selectorState);
+  }, [selectorState]);
+
   return (
     <>
       <Filter />
@@ -15,6 +35,7 @@ const State = () => {
       <hr />
       <h3>ACA SE MUESTRA LOS STADOS</h3>
       <h1>State</h1>
+      <Lists items={items} />
     </>
   );
 };
