65100a8893b6e49f714679be80514f174bec03f9
migayalach <ayalachavezmiguel@gmail.com>
2023-10-04 12:44:03 -0400

    form area

--- client_form/src/Views/Area/Area.jsx
+++ client_form/src/Views/Area/Area.jsx
@@ -1,13 +1,35 @@
 // COMPONET'S
+import FormArea from "../Forms/FormArea/FormArea";
+import Filter from "../../Components/Filter/Filter";
+import Lists from "../../Components/Lists";
 
 // HOOK'S
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+// JAVASCRIP
+import { clearName } from "../../Utils/clearFunctions";
+
+// REDUX
+import { getAllArea } from "../../Redux/actions";
 
 // STYLESHEET'S
 
 const Area = () => {
+  const dispatch = useDispatch();
+  const selectorArea = useSelector((state) => state.area);
+
+  useEffect(() => {
+    dispatch(getAllArea());
+  }, [dispatch]);
+
   return (
     <>
-      <h1>Area</h1>
+      <Filter />
+      <hr />
+      <FormArea />
+      <hr />
+      <Lists items={clearName(selectorArea)} text={"Área"} />
     </>
   );
 };
---
+++ client_form/src/Views/Forms/FormArea/FormArea.jsx
@@ -0,0 +1,59 @@
+// COMPONET'S
+import { ButtonAccept } from "../../../Components/ButtonAccept";
+
+// HOOK'S
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+
+// REDUX
+import {addArea} from "../../../Redux/actions";
+
+// JAVASCRIP
+import { validationName } from "../../../Validations/validationName";
+
+// STYLESHEET'S
+import "./form-area.css";
+
+const FormArea = () => {
+  const dispatch = useDispatch();
+
+  const [userData, setUserData] = useState({
+    name: "",
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (event) => {
+    setUserData({
+      ...userData,
+      [event.target.name]: event.target.value,
+    });
+    setErrors(
+      validationName({ ...userData, [event.target.name]: event.target.value })
+    );
+  };
+
+  const handleAccept = (event) => {
+    const name = userData.name;
+    event.preventDefault();
+    dispatch(addArea({ nameUnit: name }));
+  };
+
+  return (
+    <form className="form-component">
+      <label htmlFor="nombre">Nombre: </label>
+      <input
+        type="text"
+        value={userData.name}
+        name="name"
+        onChange={handleChange}
+      />
+      {errors.name && <p className="error">{errors.name}</p>}
+      {!errors.name && (
+        <ButtonAccept label={"Aceptar"} onClickAccept={handleAccept} />
+      )}
+    </form>
+  );
+};
+
+export default FormArea;
