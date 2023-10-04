2427da4d534fee7a0b4c2ca832bea88a87499714
migayalach <ayalachavezmiguel@gmail.com>
2023-10-04 11:40:07 -0400

    mostrar

--- client_form/src/Views/Procedures/Procedures.jsx
+++ client_form/src/Views/Procedures/Procedures.jsx
@@ -1,13 +1,32 @@
 // COMPONET'S
+import FormProcedures from "../Forms/FormProcedures/FormProcedures";
+import Filter from "../../Components/Filter/Filter";
+import Lists from "../../Components/Lists";
 
 // HOOK'S
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+// REDUX
+import { getAllProcedures } from "../../Redux/actions";
 
 // STYLESHEET'S
 
 const Procedures = () => {
+  const dispatch = useDispatch();
+  const selectorProcedures = useSelector((state) => state.procedures);
+
+  useEffect(() => {
+    dispatch(getAllProcedures());
+  }, [dispatch]);
+
   return (
     <>
-      <h1>Procedures</h1>
+      <Filter />
+      <hr />
+      <FormProcedures />
+      <hr />
+      <Lists items={selectorProcedures} text={"Procedimientos"} />
     </>
   );
 };
