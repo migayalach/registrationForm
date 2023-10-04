ead8b134a7e08726eb2bc854a4369858efc52d56
migayalach <ayalachavezmiguel@gmail.com>
2023-10-03 23:05:10 -0400

    envio de un indice

--- client_form/src/Components/Lists.jsx
+++ client_form/src/Components/Lists.jsx
@@ -1,13 +1,27 @@
 // COMPONET'S
+import List from "./List";
 
 // HOOK'S
 
 // STYLESHEET'S
 
-const Lists = () => {
+const Lists = ({ items, view, editState, deleteState, text }) => {
   return (
     <>
-      <h1>Lists</h1>
+      <h1>Lista de {text}</h1>
+      {items?.map(({ idState, name }, index) => {
+        return (
+          <List
+            key={index}
+            index={index + 1}
+            idState={idState}
+            name={name}
+            view={view}
+            edit={editState}
+            delete={deleteState}
+          />
+        );
+      })}
     </>
   );
 };
