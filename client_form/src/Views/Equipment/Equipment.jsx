056f712c79db71c37a9f67fbf9e5a0801dbe5a8c
migayalach <ayalachavezmiguel@gmail.com>
2023-10-05 02:54:22 -0400

    mustra los datos

--- client_form/src/Views/Equipment/Equipment.jsx
+++ client_form/src/Views/Equipment/Equipment.jsx
@@ -1,13 +1,34 @@
 // COMPONET'S
+import FormEquipment from "../Forms/FormEquipment/FormEquipment";
+import Filter from "../../Components/Filter/Filter";
+import Lists from "../../Components/Lists";
 
 // HOOK'S
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+// REDUX
+import { getAllEquipment } from "../../Redux/actions";
+
+// JAVASCRIP
 
 // STYLESHEET'S
 
 const Equipment = () => {
+  const dispatch = useDispatch();
+  const selectorEquipment = useSelector((state) => state.equipment);
+
+  useEffect(() => {
+    dispatch(getAllEquipment());
+  }, [dispatch]);
+
   return (
     <>
-      <h1>Equipment</h1>
+      <Filter />
+      <hr />
+      <FormEquipment />
+      <hr />
+      <Lists items={selectorEquipment} text={"Equipo"} />
     </>
   );
 };
