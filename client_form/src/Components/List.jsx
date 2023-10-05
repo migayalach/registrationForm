79ac08a105418f3fdc091f2344579a8d9176fd3e
migayalach <ayalachavezmiguel@gmail.com>
2023-10-05 06:03:36 -0400

    id's recividos

--- client_form/src/Components/List.jsx
+++ client_form/src/Components/List.jsx
@@ -4,14 +4,39 @@
 
 // STYLESHEET'S
 
-const List = ({ index, idState, name, view, editState, deleteState }) => {
+const List = ({
+  index,
+  idState,
+  idProcedures,
+  idEquipment,
+  idUnit,
+  name,
+  idUser,
+  nroIdentification,
+  phone,
+  email,
+  charge,
+  view,
+  editState,
+  deleteState,
+}) => {
   return (
     <>
       <table border={1}>
         <tbody>
           <tr>
             <td>{index}</td>
+            {/* ID'S */}
+            {idState && <td>{idState}</td>}
+            {idProcedures && <td>{idProcedures}</td>}
+            {idEquipment && <td>{idEquipment}</td>}
+            {idUnit && <td>{idUnit}</td>}
+            {idUser && <td>{idUser}</td>}
             <td>{name}</td>
+            {nroIdentification && <td>{nroIdentification}</td>}
+            {phone && <td>{phone}</td>}
+            {email && <td>{email}</td>}
+            {charge && <td>{charge}</td>}
             <td>
               <button>Ver</button>
             </td>
