0f7c629abb525dfa477a598faad4aa800890d43e
migayalach <ayalachavezmiguel@gmail.com>
2023-10-05 04:51:32 -0400

    hasta aqui todo da normal al mostrar los datos

--- client_form/src/Components/Lists.jsx
+++ client_form/src/Components/Lists.jsx
@@ -1,34 +1,3 @@
-// // COMPONET'S
-// import List from "./List";
-// import Pagintation from "./Pagination/Pagintation";
-
-// // HOOK'S
-
-// // STYLESHEET'S
-
-// const Lists = ({ items, view, editState, deleteState, text }) => {
-//   return (
-//     <>
-//       <h1>Lista de {text}</h1>
-//       {items?.map(({ idState, name }, index) => {
-//         return (
-//           <List
-//             key={index}
-//             index={index + 1}
-//             idState={idState}
-//             name={name}
-//             view={view}
-//             edit={editState}
-//             delete={deleteState}
-//           />
-//         );
-//       })}
-//     </>
-//   );
-// };
-
-// export default Lists;
-
 // COMPONET'S
 import List from "./List";
 import Pagination from "./Pagination/Pagination";
