857e0e272d701a60c16ca9504a93fb2b83358459
migayalach <ayalachavezmiguel@gmail.com>
2023-10-05 02:52:53 -0400

    creacion desde el formulario para equipos, listo

--- client_form/src/Views/Forms/FormEquipment/FormEquipment.jsx
+++ client_form/src/Views/Forms/FormEquipment/FormEquipment.jsx
@@ -1,14 +1,58 @@
 // COMPONET'S
+import { ButtonAccept } from "../../../Components/ButtonAccept";
 
 // HOOK'S
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+
+// REDUX
+import { addEquipment } from "../../../Redux/actions";
+
+// JAVASCRIP
+import { validationName } from "../../../Validations/validationName";
 
 // STYLESHEET'S
+import "./form-equipment.css";
 
 const FormEquipment = () => {
+  const dispatch = useDispatch();
+
+  const [userData, setUserData] = useState({
+    name: "",
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (event) => {
+    setUserData({
+      ...userData,
+      [event.target.name]: event.target.value,
+    });
+    setErrors(
+      validationName({ ...userData, [event.target.name]: event.target.value })
+    );
+  };
+
+  const handleAccept = (event) => {
+    const name = userData.name;
+    event.preventDefault();
+    dispatch(addEquipment({ name }));
+  };
+
   return (
-    <>
-      <h1>FormEquipment</h1>
-    </>
+    <form className="form-component">
+      <label htmlFor="nombre">Nombre: </label>
+      <input
+        type="text"
+        value={userData.name}
+        name="name"
+        onChange={handleChange}
+      />
+      {errors.name && <p className="error">{errors.name}</p>}
+      {!errors.name && (
+        <ButtonAccept label={"Aceptar"} onClickAccept={handleAccept} />
+      )}
+    </form>
   );
 };
 
