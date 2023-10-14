12359e603ed320a40e2137ccd7177c0ab43c49ca
migayalach <ayalachavezmiguel@gmail.com>
2023-10-14 17:02:27 -0400

    LIMPIAR DATOS QUE SE ENVIARAN AL BACK, funciones extra

--- client_form/src/Utils/clearFunctions.js
+++ client_form/src/Utils/clearFunctions.js
@@ -3,3 +3,28 @@ export const clearName = (data) =>
     idUnit,
     name: nameUnit,
   }));
+
+
+// LIMPIEZA FORM HIGH
+export const clearDataHigh = (data) => {
+  const obj = {
+    idUser: data[0]?.idUser,
+    name: "alta",
+    credendia: data.dataSend,
+    equipment: clearArray([
+      data.equipment.idEquipment ? data.equipment : null,
+      data.equipment2.idEquipment ? data.equipment2 : null,
+    ]),
+  };
+  return obj;
+};
+
+const clearArray = (array) => {
+  const aux = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] !== null) {
+      aux.push(array[i]);
+    }
+  }
+  return aux;
+};
