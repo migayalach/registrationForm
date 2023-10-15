a83b89581694f287d541393facb31a783d4ff143
migayalach <ayalachavezmiguel@gmail.com>
2023-10-15 01:43:24 -0400

    datos recividos, perfecto

--- server/src/handlers/formHandler.js
+++ server/src/handlers/formHandler.js
@@ -9,23 +9,51 @@ const {
   delFomr,
 } = require("../controllers/formControllers");
 
+// // POSTMAN
+// {
+//   "idUser": 3,
+//   "idState": 2,
+//   "nameProcedures": "alta",
+//   "credential": [
+//     { "idCredential": "1", "check": true },
+//     { "idCredential": "2", "check": true },
+//     { "idCredential": "3", "check": true },
+//     { "idCredential": "4", "check": true },
+//     { "idCredential": "5", "check": true }
+//   ],
+//   "equipment": [
+//     {
+//       "idEquipment": "1",
+//       "host": "255.255.225.1",
+//       "direccionIP": "192.166.0.1",
+//       "etiquetaControl": true
+//     },
+
+//     {
+//       "idEquipment": "2",
+//       "host": "255.255.225.255",
+//       "direccionIP": "192.166.100.102",
+//       "etiquetaControl": true
+//     }
+//   ]
+// }
+
+// idEquipment,    //ok [{}]
+// idUserApi,      //ok int
+// idState,        //ok int
+// idProcedures,   //ok string
+// idCredential,   //ok [{}]
+
 const postForm = async (request, response) => {
-  const {
-    dateStart,
-    idEquipment,
-    idUserApi,
-    idState,
-    idProcedures,
-    idCredential,
-  } = request.body;
+  const { idUser, idState, nameProcedures, equipment, credential } =
+    request.body;
   try {
     const resCreateForm = await createForm(
-      dateStart,
-      idEquipment,
-      idUserApi,
+      idUser,
       idState,
-      idProcedures,
-      idCredential
+      nameProcedures,
+      equipment,
+      credential
     );
     response.status(SUCCESS).json({ create: true, resCreateForm });
   } catch (error) {
@@ -43,10 +71,10 @@ const getFormId = async (request, response) => {
   }
 };
 
-const getFormName =async (request, response) => {
+const getFormName = async (request, response) => {
   try {
     const responseForm = await getAllForm();
-    response.status(SUCCESS).json(responseForm)
+    response.status(SUCCESS).json(responseForm);
   } catch (error) {
     response.status(ERROR).json({ error: error.message });
   }
