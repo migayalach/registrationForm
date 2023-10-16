5b79a53ce566db63d5d9d26db68602752723eda6
migayalach <ayalachavezmiguel@gmail.com>
2023-10-16 11:41:08 -0400

    solo agrege una imagen al about y a error

--- client_form/src/Views/About/About.jsx
+++ client_form/src/Views/About/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
       <h1>Sobre mi</h1>
       <h1>Hola mi nombre es Daniela Sole</h1>
       <img
-        src="https://scontent.flpb2-1.fna.fbcdn.net/v/t39.30808-6/218725548_2987413471506300_6716575543868373728_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=52f669&_nc_ohc=KxZiLv-qkfkAX-wBzxh&_nc_ht=scontent.flpb2-1.fna&oh=00_AfDWm0PElzgSuUYv8qj7DV20d3Erdam5NS61wCr3ZZo4pA&oe=652313F3"
+        src="https://scontent.flpb2-1.fna.fbcdn.net/v/t39.30808-6/393399146_832791881875906_9000518948385316679_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=5f2048&_nc_ohc=hRbE9-OgHQgAX9x-TP1&_nc_ht=scontent.flpb2-1.fna&oh=00_AfAh_PJxs8ylS6MW195SLU8iXkdAxSJhujDXiGq_Fdc_TQ&oe=6531AB91"
         alt="foto"
       />
       <h3>I am a backend developer</h3>
--- client_form/src/Views/Error/Error.jsx
+++ client_form/src/Views/Error/Error.jsx
@@ -8,6 +8,10 @@ const Error = () => {
   return (
     <>
       <h1>Error :C</h1>
+      <img
+        src="https://scontent.flpb2-1.fna.fbcdn.net/v/t39.30808-6/393399146_832791881875906_9000518948385316679_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=5f2048&_nc_ohc=hRbE9-OgHQgAX9x-TP1&_nc_ht=scontent.flpb2-1.fna&oh=00_AfAh_PJxs8ylS6MW195SLU8iXkdAxSJhujDXiGq_Fdc_TQ&oe=6531AB91"
+        alt="foto"
+      />
     </>
   );
 };
