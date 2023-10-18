7d2c1d5a9ffebb089c9b8d370dd1faeebf3c6cd7
migayalach <ayalachavezmiguel@gmail.com>
2023-10-17 22:23:29 -0400

    lo comente porque vi que tenia retraso de lleada de informacion y di solucion en list

--- client_form/src/Components/ButtonEdit.jsx
+++ client_form/src/Components/ButtonEdit.jsx
@@ -1,3 +1,30 @@
-export const ButtonEdit = ({ label, onClickAccept }) => {
-  return <button onClick={onClickAccept}>{label}</button>;
-};
+// // COMPONENT'S
+// import FormArea from "../Views/Forms/FormArea/FormArea";
+
+// // HOOS'K
+// import { useState, useEffect } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+
+// // REDUX
+// import { seachIdArea } from "../Redux/actions";
+
+// export const ButtonEdit = ({ id }) => {
+//   const dispatch = useDispatch();
+//   const selecInfo = useSelector((state) => state.aux);
+//   const [idData, setIdData] = useState([]);
+  
+//   const [data, setData] = useState([]);
+  
+//   useEffect(() => {
+    
+//   }, []);
+
+//   const handleMessage = (id) => {
+//     dispatch(seachIdArea(id));
+//     setIdData(id);
+//     alert(id)
+//   };
+
+//   console.log(data);
+//   return <button onClick={() => handleMessage(id)}>EDITAR</button>;
+// };
