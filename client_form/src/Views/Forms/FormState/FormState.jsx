d2f2a85fb6c9fcc959653c99aa893e7ea003c7dd
migayalach <ayalachavezmiguel@gmail.com>
2023-10-20 09:40:16 -0400

    editacion con exito

--- client_form/src/Views/Forms/FormState/FormState.jsx
+++ client_form/src/Views/Forms/FormState/FormState.jsx
@@ -2,23 +2,24 @@
 import { ButtonAccept } from "../../../Components/ButtonAccept";
 
 // HOOK'S
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 // REDUX
-import { addState } from "../../../Redux/actions";
+import { addState, putState } from "../../../Redux/actions";
 
 // JAVASCRIP
 import { validationName } from "../../../Validations/validationName";
 
 // STYLESHEET'S
-import styles from './FormState.module.css'; 
+import styles from "./FormState.module.css";
 
 const FormState = () => {
   const dispatch = useDispatch();
-
+  const selectorState = useSelector((state) => state.aux);
+  const initialName = selectorState.length ? selectorState[0].name : "";
   const [userData, setUserData] = useState({
-    name: "",
+    name: initialName,
   });
 
   const [errors, setErrors] = useState({});
@@ -36,32 +37,33 @@ const FormState = () => {
   const handleAccept = (event) => {
     const name = userData.name;
     event.preventDefault();
-    dispatch(addState({ name }));
+    selectorState[0]?.idState
+      ? dispatch(putState({ idState: selectorState[0].idState, name }))
+      : dispatch(addState({ name }));
   };
+
+  useEffect(() => {
+    setUserData({ name: initialName });
+  }, [initialName]);
 
   return (
     <form>
       <div>
-      <label htmlFor="nombre">Nombre: </label>
-      <input
-        type="text"
-        value={userData.name}
-        name="name"
-        onChange={handleChange}
-      />
+        <label htmlFor="nombre">Nombre: </label>
+        <input
+          type="text"
+          value={userData.name}
+          name="name"
+          onChange={handleChange}
+        />
       </div>
-      {errors.name && <p >{errors.name}</p>}
-      <div className={styles['button-container']}>
-        <button
-          className={styles['button-accept']}
-          onClick={handleAccept}
-        >
+      {errors.name && <p>{errors.name}</p>}
+      <div className={styles["button-container"]}>
+        <button className={styles["button-accept"]} onClick={handleAccept}>
           Aceptar
         </button>
       </div>
     </form>
-
-    
   );
 };
 
