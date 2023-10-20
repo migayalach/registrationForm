1ad281c8bd9768f830d3ed3dd53c773c3c767a51
migayalach <ayalachavezmiguel@gmail.com>
2023-10-20 04:00:29 -0400

    se paso por parametros a idEquipment

--- client_form/src/Components/ButtonDelete.jsx
+++ client_form/src/Components/ButtonDelete.jsx
@@ -2,18 +2,19 @@
 import { useDispatch } from "react-redux";
 
 // REDUX
-import { deleteArea } from "../Redux/actions";
+import { deleteArea, deleteEquipment } from "../Redux/actions";
 
-export const ButtonDelete = ({ id, flag }) => {
+export const ButtonDelete = ({ id, idEquipment, flag }) => {
   const dispatch = useDispatch();
 
-  const handleDelete = (id) => {
+  const handleDelete = () => {
     flag === "area" && dispatch(deleteArea(id));
+    flag === "equipment" && dispatch(deleteEquipment(idEquipment));
   };
 
   return (
     <>
-      <button onClick={() => handleDelete(id)}>ELIMINAR</button>
+      <button onClick={() => handleDelete()}>ELIMINAR</button>
     </>
   );
 };
