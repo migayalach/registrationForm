e642ce4b1f2a4b28618ef8b701e548dd2da90ba4
migayalach <ayalachavezmiguel@gmail.com>
2023-10-20 09:39:47 -0400

    accion editar lista, pasando el flag por list

--- client_form/src/Components/ButtonEdit.jsx
+++ client_form/src/Components/ButtonEdit.jsx
@@ -2,22 +2,22 @@
 import { useDispatch } from "react-redux";
 
 // REDUX
-import { seachIdArea, getIdEquipment } from "../Redux/actions";
+import { seachIdArea, getIdEquipment, getIdState } from "../Redux/actions";
 
-export const ButtonEdit = ({ id, idEquipment, flag }) => {
+export const ButtonEdit = ({ id, idEquipment, idState, flag }) => {
   const dispatch = useDispatch();
 
   const handleEdit = (id) => {
     flag === "area" && dispatch(seachIdArea(id));
     flag === "equipment" && dispatch(getIdEquipment(idEquipment));
+    flag === "state" && dispatch(getIdState(idState))
   };
 
   return (
     <>
       {id && <button onClick={() => handleEdit(id)}>EDITAR</button>}
-      {idEquipment && (
-        <button onClick={() => handleEdit(idEquipment)}>EDITAR</button>
-      )}
+      {idEquipment && (<button onClick={() => handleEdit(idEquipment)}>EDITAR</button>)}
+      {idState && <button onClick={() => handleEdit(idEquipment)}>EDITAR</button>}
     </>
   );
 };
