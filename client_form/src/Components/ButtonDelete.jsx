6310b790425f3ea955f2c02766a939dfa89292e2
migayalach <ayalachavezmiguel@gmail.com>
2023-10-20 22:31:52 -0400

    eliminar y editar listo

--- client_form/src/Components/ButtonDelete.jsx
+++ client_form/src/Components/ButtonDelete.jsx
@@ -2,20 +2,34 @@
 import { useDispatch } from "react-redux";
 
 // REDUX
-import { deleteArea, deleteEquipment, deleteState } from "../Redux/actions";
+import {
+  deleteArea,
+  deleteEquipment,
+  deleteState,
+  deleteProcedures,
+} from "../Redux/actions";
 
-export const ButtonDelete = ({ id, idEquipment, idState, flag }) => {
+export const ButtonDelete = ({
+  id,
+  idEquipment,
+  idState,
+  idProcedures,
+  flag,
+}) => {
   const dispatch = useDispatch();
 
   const handleDelete = () => {
     flag === "area" && dispatch(deleteArea(id));
     flag === "equipment" && dispatch(deleteEquipment(idEquipment));
     flag === "state" && dispatch(deleteState(idState));
+    flag === "procedures" && dispatch(deleteProcedures(idProcedures));
   };
 
   return (
     <>
-      <button onClick={() => handleDelete()}>ELIMINAR</button>
+      {
+        flag !=="userApi" && <button onClick={() => handleDelete()}>ELIMINAR</button>
+      }
     </>
   );
 };
