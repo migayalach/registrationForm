5a9b0cb4f46e25354d9d1381981257bf9b12a011
migayalach <ayalachavezmiguel@gmail.com>
2023-10-21 23:18:18 -0400

    ruta credencial lista

--- server/src/routes/credentialRoute.js
+++ server/src/routes/credentialRoute.js
@@ -3,7 +3,7 @@ const {
   postCredential,
   getCredentialId,
   getCredentialName,
-  // putCredential,
+  putCredential,
   deleteCredential,
 } = require("../handlers/credentialHandler");
 const credentialRouter = Router();
@@ -11,7 +11,7 @@ const credentialRouter = Router();
 credentialRouter.post("/", postCredential);
 credentialRouter.get("/:idCredential", getCredentialId);
 credentialRouter.get("/", getCredentialName);
-// credentialRouter.put("/", putCredential);
+credentialRouter.put("/", putCredential);
 credentialRouter.delete("/:idCredential", deleteCredential);
 
 module.exports = credentialRouter;
