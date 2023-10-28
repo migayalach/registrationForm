89027750c424baa142ac4774f84c9618553fd45f
migayalach <ayalachavezmiguel@gmail.com>
2023-10-28 17:14:23 -0400

    formulario mejorado y listo para enviar datos al back y este lo guarede en la base de datos

--- client_form/src/Views/Forms/Form/Form.jsx
+++ client_form/src/Views/Forms/Form/Form.jsx
@@ -1,24 +1,48 @@
 // COMPONET'S
 import { NavLink } from "react-router-dom";
 import Filter from "../../../Components/Filter/Filter";
-
 import DataServerPrublic from "../../../Components/DataServerPrublic";
 import DataEquipment from "../../../Components/DataEquipment";
 import DataCredential from "../../../Components/DataCredential";
+import SelectState from "../../../Components/SelectState";
+import SelectProcedures from "../../../Components/SelectProcedures";
 
 // HOOK'S
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 // STYLESHEET'S
 
 const Form = () => {
+  const [data, setData] = useState({
+    userPublic: "",
+    equipment: "",
+    credential: "",
+    state: "",
+    procedure: "",
+  });
+
+  const handleData = (info, type) => {
+    setData({
+      ...data,
+      [type]: info,
+    });
+  };
+
+  useEffect(() => {}, []);
+  console.log(data);
   return (
     <div>
+      <h4>Seleccione un estado:</h4>
+      <SelectState handleData={handleData} />
+      <h4>Seleccione un proceso:</h4>
+      <SelectProcedures handleData={handleData} />
       <h4>1.- Datos del servidor publico:</h4>
-      <DataServerPrublic />
+      <DataServerPrublic handleData={handleData} />
       <h4>2.- Equipos de computación:</h4>
-      <DataEquipment />
+      <DataEquipment handleData={handleData} />
       <h4>3.- Credenciales de acceso:</h4>
-      <DataCredential />
+      <DataCredential handleData={handleData} />
     </div>
   );
 };
