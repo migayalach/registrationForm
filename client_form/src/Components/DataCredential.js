07135568121a5d075174f84df312035ceb3dae92
migayalach <ayalachavezmiguel@gmail.com>
2023-10-30 00:33:54 -0400

    se inicio un statdo local llamado flag el cual se usara como un switch y este a su vez se lo usara en un useEffect para poder estar atento a los cambios y poder mostrarlo

--- client_form/src/Components/DataCredential.js
+++ client_form/src/Components/DataCredential.js
@@ -14,6 +14,7 @@ import "../StyleSheets/ListaDeTareas.css";
 
 const DataCredential = ({ handleData }) => {
   const [credenciales, setCredenciales] = useState([]);
+  const [flag, setFlag] = useState(false);
   const selectCredencial = useSelector((state) => state.auxExtra);
   const dataCredencial = selectCredencial[0]?.credential;
 
@@ -31,19 +32,18 @@ const DataCredential = ({ handleData }) => {
     setCredenciales(credencialesActualizadas);
   };
 
-  const completarTarea = (id) => {
-    const credencialesActualizadas = credenciales.map((credencial) => {
-      if (credencial.id === id) {
-        credencial.completada = !credencial.completada;
+  const completarTarea = (id, checked) => {
+    for (const i of credenciales) {
+      if (i.id === id) {
+        i.check = !checked;
       }
-      return credencial;
-    });
-    setCredenciales(credencialesActualizadas);
+    }
+    setFlag(!flag);
   };
 
   useEffect(() => {
     handleData(credenciales, "credential");
-  }, [credenciales]);
+  }, [credenciales, flag]);
 
   useEffect(() => {
     if (dataCredencial) {
