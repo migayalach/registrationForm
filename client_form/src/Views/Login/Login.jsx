2314442d95c35331a46028cdf231f039cc5efcb0
migayalach <ayalachavezmiguel@gmail.com>
2023-10-30 23:28:30 -0400

    se agrego un formulario para poder enviar los datos de manera correcta

--- client_form/src/Views/Login/Login.jsx
+++ client_form/src/Views/Login/Login.jsx
@@ -3,7 +3,7 @@
 // HOOK'S
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 
 //REDUX
 import { login } from "../../Redux/actions";
@@ -11,12 +11,11 @@ import { login } from "../../Redux/actions";
 //JAVASCRIP
 
 // STYLESHEET'S
-import styles from "./login.module.css";
+import "./login.css";
 
 const Login = () => {
-  const navigate = useNavigate();
-  // const dispatch = useDispatch();
-  // const selectAux = useSelector((state) => state.aux);
+  // const navigate = useNavigate();
+  const dispatch = useDispatch();
   const [showPassword, setShowPassword] = useState(false);
   const [inputData, setInputData] = useState({
     emailUser: "",
@@ -25,8 +24,8 @@ const Login = () => {
 
   const handleLogin = (event) => {
     event.preventDefault();
-    // dispatch(login(inputData));
-    navigate("/home");
+    dispatch(login(inputData));
+    // navigate("/home");
   };
 
   const handleChange = (event) => {
@@ -41,8 +40,8 @@ const Login = () => {
   }, []);
 
   return (
-    <div className={styles["login-container"]}>
-      <div className={styles["login-form"]}>
+    <div className="login-container">
+      <form className="login-form" onSubmit={handleLogin}>
         <h1>Formulario de Autenticación</h1>
         <div>
           <input
@@ -51,35 +50,36 @@ const Login = () => {
             value={inputData.emailUser}
             onChange={handleChange}
             placeholder="Ingrese su Correo"
-            className={styles["input"]}
+            className="input"
           />
         </div>
-        <div>
+        <div className="password-input">
           <input
             type={showPassword ? "text" : "password"}
             name="password"
             value={inputData.password}
             onChange={handleChange}
             placeholder="Contraseña"
-            className={styles["input"]}
+            className="input"
           />
           <button
+            type="button"
             onClick={() => setShowPassword(!showPassword)}
-            className={`${styles["toggle-password-button"]} ${
-              showPassword ? styles["show-password"] : styles["hide-password"]
+            className={`toggle-password-button ${
+              showPassword ? "show-password" : "hide-password"
             }`}
           >
-            Mostrar/ocultar contraseña
+            {showPassword ? "Ocultar" : "Mostrar"} contraseña
           </button>
         </div>
-        <button onClick={handleLogin} className={styles["login-button"]}>
+        <button type="submit" className="login-button">
           Login
         </button>
-      </div>
-      <div className={styles["welcome-message"]}>
+      </form>
+      <div className="welcome-message">
         <p>
           Bienvenido al{" "}
-          <span className={styles["highlight-text"]}>
+          <span className="highlight-text">
             Sistema de Control de Altas y Bajas
           </span>
         </p>
@@ -90,3 +90,50 @@ const Login = () => {
 };
 
 export default Login;
+
+// // viejo login
+// <div className={styles["login-container"]}>
+//   <div className={styles["login-form"]}>
+//     <h1>Formulario de Autenticación</h1>
+//     <div>
+//       <input
+//         type="text"
+//         name="emailUser"
+//         value={inputData.emailUser}
+//         onChange={handleChange}
+//         placeholder="Ingrese su Correo"
+//         className={styles["input"]}
+//       />
+//     </div>
+//     <div>
+//       <input
+//         type={showPassword ? "text" : "password"}
+//         name="password"
+//         value={inputData.password}
+//         onChange={handleChange}
+//         placeholder="Contraseña"
+//         className={styles["input"]}
+//       />
+//       <button
+//         onClick={() => setShowPassword(!showPassword)}
+//         className={`${styles["toggle-password-button"]} ${
+//           showPassword ? styles["show-password"] : styles["hide-password"]
+//         }`}
+//       >
+//         Mostrar/ocultar contraseña
+//       </button>
+//     </div>
+//     <button onClick={handleLogin} className={styles["login-button"]}>
+//       Login
+//     </button>
+//   </div>
+//   <div className={styles["welcome-message"]}>
+//     <p>
+//       Bienvenido al{" "}
+//       <span className={styles["highlight-text"]}>
+//         Sistema de Control de Altas y Bajas
+//       </span>
+//     </p>
+//     <h4>Ministerio de la Presidencia</h4>
+//   </div>
+// </div>
