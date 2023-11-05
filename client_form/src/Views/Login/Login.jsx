4317f6d4d81efcfd2a087a0ad89071e9dc23bb78
migayalach <ayalachavezmiguel@gmail.com>
2023-11-05 13:02:19 -0400

    se llama al componente formLOgin

--- client_form/src/Views/Login/Login.jsx
+++ client_form/src/Views/Login/Login.jsx
@@ -1,91 +1,24 @@
 // COMPONET'S
+import FormLogin from "../Forms/FormLogin/FormLogin";
 
 // HOOK'S
-import { useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import { useEffect } from "react";
 
 //REDUX
-import { login } from "../../Redux/actions";
 
 //JAVASCRIP
-import validationLogin from "./validationLogin";
 
 // STYLESHEET'S
 import "./login.css";
 
 const Login = () => {
-  // const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const [showPassword, setShowPassword] = useState(false);
-  const [errors, setErros] = useState({});
-  const [inputData, setInputData] = useState({
-    emailUser: "",
-    password: "",
-  });
-
-  const handleLogin = (event) => {
-    event.preventDefault();
-    dispatch(login(inputData));
-    // navigate("/home");
-  };
-
-  const handleChange = (event) => {
-    setInputData({
-      ...inputData,
-      [event.target.name]: event.target.value,
-    });
-    setErros(
-      validationLogin({ ...inputData, [event.target.name]: event.target.value })
-    );
-  };
-
   useEffect(() => {
     window.history.replaceState(null, null, "/");
   }, []);
 
   return (
     <div className="login-container">
-      <form className="login-form" onSubmit={handleLogin}>
-        <h1>Formulario de Autenticación</h1>
-        <div>
-          <input
-            type="text"
-            name="emailUser"
-            value={inputData.emailUser}
-            onChange={handleChange}
-            placeholder="Ingrese su Correo"
-            className="input"
-          />
-          {errors.emailUser && <p className="error">{errors.emailUser}</p>}
-        </div>
-        <div className="password-input">
-          <input
-            type={showPassword ? "text" : "password"}
-            name="password"
-            value={inputData.password}
-            onChange={handleChange}
-            placeholder="Contraseña"
-            className="input"
-          />
-          {errors.password && <p className="error">{errors.password}</p>}
-          <button
-            type="button"
-            onClick={() => setShowPassword(!showPassword)}
-            className={`toggle-password-button ${
-              showPassword ? "show-password" : "hide-password"
-            }`}
-          >
-            {showPassword ? "Ocultar" : "Mostrar"} contraseña
-          </button>
-        </div>
-
-        {Object.keys(errors).length < 2 && (
-          <button type="submit" className="login-button">
-            Login
-          </button>
-        )}
-      </form>
+      <FormLogin />
       <div className="welcome-message">
         <p>
           Bienvenido al{" "}
