6bf6fd519beb19dac44b9ac9d1cd0fd3fa947383
migayalach <ayalachavezmiguel@gmail.com>
2023-11-05 19:17:06 -0400

    se agrego nuevas regex para poder controlar mas campos

--- client_form/src/Utils/regex.js
+++ client_form/src/Utils/regex.js
@@ -1,1 +1,9 @@
-export const nameRegex = /^(?![0-9/*+-,])[A-Za-z][A-Za-z0-9]*$/;
+// export const nameRegex = /^(?![0-9/*+-,])[A-Za-z][A-Za-z0-9]*$/;
+export const nameRegex = /^[A-Za-z][A-Za-z0-9/*+-, ]*$/;
+export const emailRegex = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+export const passwordRegexMay_AZ = /(?=.*[A-Z])/;
+export const passwordRegexMin_az = /(?=.*[a-z])/;
+export const passwordRegexDigits = /(?=.*\d)/;
+export const passwordRegexSpecialCaracter = /(?=.*[@#$%^&+=!_])/;
+export const passwordRegexLength = /^.{8,}$/;
+export const passwordRegex = /.*\d+.*/;
