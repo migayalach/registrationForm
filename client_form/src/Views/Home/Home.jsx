4b221a3e852e0c4132c5fe1cef1408923f109b6f
migayalach <ayalachavezmiguel@gmail.com>
2023-11-05 20:12:20 -0400

    extraccion de los datos personales

--- client_form/src/Views/Home/Home.jsx
+++ client_form/src/Views/Home/Home.jsx
@@ -1,10 +1,26 @@
-import React, { useState } from "react";
+// COMPONET'S
+
+// HOOK'S
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+
+// JAVASCRIP
+
+// REDUX
+
+// STYLESHEET'S
 import styles from "./home.module.css";
 
-const Home = ({ handleSaludo }) => {
+const Home = () => {
+  const selectData = useSelector((state) => state.auxUser);
+  const name = selectData[0]?.name;
+  const unit = selectData[0]?.unit;
+
   return (
     <div className={styles["home-container"]}>
-      <h1>HOME</h1>
+      <h1>Bienvenido</h1>
+      <h3>{name}</h3>
+      <h3>Encargad@ de: {unit}</h3>
     </div>
   );
 };
