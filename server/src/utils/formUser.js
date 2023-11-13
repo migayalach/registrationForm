1f74f01737536538c20044b610373980c6d8d3a4
migayalach <ayalachavezmiguel@gmail.com>
2023-11-13 15:48:09 -0400

    funcion limpiadora

--- server/src/utils/formUser.js
+++ server/src/utils/formUser.js
@@ -1,5 +1,16 @@
 const nameClear = (obj) => obj[0];
 
+const clearResponseGet = (obj) =>
+  obj.map((form) => ({
+    idForm: form.idForm,
+    dateStart: form.dateStart,
+    dateEnd: form.dateEnd,
+    state: nameClear(form.States.map(({ name }) => name)),
+    procedure: nameClear(form.Procedures.map(({ name }) => name)),
+    nameUser: nameClear(form.UserApis.map(({ name }) => name)),
+  }));
+
 module.exports = {
   nameClear,
+  clearResponseGet,
 };
