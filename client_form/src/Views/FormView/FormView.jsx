cb77a8fbc45696d74e21bf85e2ed4e485dce11c0
migayalach <ayalachavezmiguel@gmail.com>
2023-11-13 22:25:22 -0400

    en caso de no encontrar nada se manda un mensaje personalizado

--- client_form/src/Views/FormView/FormView.jsx
+++ client_form/src/Views/FormView/FormView.jsx
@@ -10,6 +10,9 @@ import { useSelector, useDispatch } from "react-redux";
 // REDUX
 import { getAllForm } from "../../Redux/actions";
 
+// LIBRARY
+import Swal from "sweetalert2";
+
 // JAVASCRIP
 
 // STYLESHEET'S
@@ -22,11 +25,20 @@ const FormView = () => {
     dispatch(getAllForm());
   }, [dispatch]);
 
+  useEffect(() => {
+    selecForm[0]?.message === "No se encontraron datos" &&
+      Swal.fire({
+        icon: "info",
+        title: "Oops...",
+        text: "No se encontraron datos",
+      });
+  }, [selecForm]);
+
   return (
     <>
       <Form />
       <Filter />
-      <Lists items={selecForm} text={"Lista de formularios"} flag={"form"} />
+      <Lists items={selecForm} text={"formularios"} flag={"form"} />
     </>
   );
 };
