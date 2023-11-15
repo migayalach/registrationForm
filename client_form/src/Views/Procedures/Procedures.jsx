5a8956f0218dd3c8dc307a182edc89b30aab3c42
migayalach <ayalachavezmiguel@gmail.com>
2023-11-15 12:29:54 -0400

    mensaje de error, listo para uso

--- client_form/src/Views/Procedures/Procedures.jsx
+++ client_form/src/Views/Procedures/Procedures.jsx
@@ -10,15 +10,29 @@ import { useSelector, useDispatch } from "react-redux";
 // REDUX
 import { getAllProcedures } from "../../Redux/actions";
 
+// LIBRARY
+import Swal from "sweetalert2";
+
 // STYLESHEET'S
 
 const Procedures = () => {
   const dispatch = useDispatch();
   const selectorProcedures = useSelector((state) => state.procedures);
+  const errorValidate = useSelector((state) => state.errors);
 
   useEffect(() => {
     dispatch(getAllProcedures());
   }, [dispatch]);
+
+  useEffect(() => {
+    errorValidate &&
+      errorValidate.procedure === false &&
+      Swal.fire({
+        icon: "warning",
+        title: "Oops...",
+        text: errorValidate.error,
+      });
+  }, [errorValidate]);
 
   return (
     <>
@@ -26,7 +40,11 @@ const Procedures = () => {
       <hr /> */}
       <FormProcedures />
       <hr />
-      <Lists items={selectorProcedures} text={"Procedimientos"} flag={"procedures"}/>
+      <Lists
+        items={selectorProcedures}
+        text={"Procedimientos"}
+        flag={"procedures"}
+      />
     </>
   );
 };
