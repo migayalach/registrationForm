d670e8861d506ff3b606ce88009e90c642d21e59
migayalach <ayalachavezmiguel@gmail.com>
2023-11-15 12:25:56 -0400

    implementacion de try catch

--- server/src/Middleware/equipmentMiddleware.js
+++ server/src/Middleware/equipmentMiddleware.js
@@ -3,32 +3,33 @@ const ERROR = 400;
 const validateEquipmentData = (request, response, next) => {
   const { name } = request.body;
   const minLength = 4;
-  if (!name)
+  try {
+    if (!name) throw Error(`Por favor ingrese un nombre`);
+    if (name.length < minLength)
+      throw Error(`El nombre es muy corto para poder ser valido`);
+    return next();
+  } catch (error) {
     return response
       .status(ERROR)
-      .json({ error: `Por favor ingrese un nombre` });
-  if (name.length < minLength)
-    return response
-      .status(ERROR)
-      .json({ error: `El nombre es muy corto para poder ser valido` });
-  next();
+      .json({ equipment: false, error: error.message });
+  }
 };
 
 const validateEquipmentDataPut = (request, response, next) => {
   const { idEquipment, name } = request.body;
-  if (!idEquipment)
+  try {
+    if (!idEquipment) throw Error(`Falta el identificador de lo que busca`);
+    if (!Number.isInteger(+idEquipment))
+      throw Error(
+        `Necesitamos saber el identificador unico para poder buscarlo`
+      );
+    if (!name) throw Error(`Por favor ingrese un nombre`);
+    return next();
+  } catch (error) {
     return response
       .status(ERROR)
-      .json({ error: `Falta el identificador de lo que busca` });
-  if (!Number.isInteger(+idEquipment))
-    return response.status(ERROR).json({
-      error: `Necesitamos saber el identificador unico para poder buscarlo`,
-    });
-  if (!name)
-    return response
-      .status(ERROR)
-      .json({ error: `Por favor ingrese un nombre` });
-  next();
+      .json({ equipment: false, error: error.message });
+  }
 };
 
 module.exports = { validateEquipmentData, validateEquipmentDataPut };
