7907c896d1474fabdd67ace90a77201862f54b0a
migayalach <ayalachavezmiguel@gmail.com>
2023-11-15 12:27:22 -0400

    implementacion de try catch

--- server/src/Middleware/unitMiddleware.js
+++ server/src/Middleware/unitMiddleware.js
@@ -3,32 +3,29 @@ const ERROR = 400;
 const validateUnitData = (request, response, next) => {
   const { nameUnit } = request.body;
   const minLength = 4;
-  if (!nameUnit)
-    return response
-      .status(ERROR)
-      .json({ error: `Por favor ingrese un nombre` });
-  if (nameUnit.length < minLength)
-    return response
-      .status(ERROR)
-      .json({ error: `El nombre es muy corto para poder ser valido` });
-  next();
+  try {
+    if (!nameUnit) throw Error(`Por favor ingrese un nombre`);
+    if (nameUnit.length < minLength)
+      throw Error(`El nombre es muy corto para poder ser valido`);
+    return next();
+  } catch (error) {
+    return response.status(ERROR).json({ unit: false, error: error.message });
+  }
 };
 
 const validateUnitDataPut = (request, response, next) => {
   const { idUnit, nameUnit } = request.body;
-  if (!idUnit)
-    return response
-      .status(ERROR)
-      .json({ error: `Falta el identificador de lo que busca` });
-  if (!Number.isInteger(+idUnit))
-    return response.status(ERROR).json({
-      error: `Necesitamos saber el identificador unico para poder buscarlo`,
-    });
-  if (!nameUnit)
-    return response
-      .status(ERROR)
-      .json({ error: `Por favor ingrese un nombre` });
-  next();
+  try {
+    if (!idUnit) throw Error(`Falta el identificador de lo que busca`);
+    if (!Number.isInteger(+idUnit))
+      throw Error(
+        `Necesitamos saber el identificador unico para poder buscarlo`
+      );
+    if (!nameUnit) throw Error(`Por favor ingrese un nombre`);
+    return next();
+  } catch (error) {
+    return response.status(ERROR).json({ unit: false, error: error.message });
+  }
 };
 
 module.exports = { validateUnitData, validateUnitDataPut };
