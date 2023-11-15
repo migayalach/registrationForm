4435bc6e0b1031ac6918f1c88950cda1bfd38056
migayalach <ayalachavezmiguel@gmail.com>
2023-11-14 21:38:04 -0400

    este middleware solo se usa para controlar los id

--- server/src/Middleware/toolsMiddleware.js
+++ server/src/Middleware/toolsMiddleware.js
@@ -1,39 +1,27 @@
 const ERROR = 400;
 
-const validateData = (request, response, next) => {
-  const { name } = request.body;
-  if (!name)
-    return response
-      .status(ERROR)
-      .json({ error: `Por favor ingrese un nombre` });
-  if (name.length < 4)
-    return response
-      .status(ERROR)
-      .json({ error: `El nombre es muy corto para poder ser valido` });
-  next();
-};
-
 const validateId = (request, response, next) => {
-  const { idState } = request.params;
-  if (!Number.isInteger(+idState))
+  const {
+    idState,
+    idProcedures,
+    idEquipment,
+    idUnit,
+    idCredential,
+    idUser,
+    idForm,
+  } = request.params;
+  if (
+    !Number.isInteger(
+      +idState ||
+        +idProcedures ||
+        +idEquipment ||
+        +idUnit ||
+        +idCredential ||
+        +idUser ||
+        +idForm
+    )
+  )
     return response.status(ERROR).json({ error: `No se encontro lo busca` });
-  next();
-};
-
-const validateDataPut = (request, response, next) => {
-  const { idState, name } = request.body;
-  if (!idState)
-    return response
-      .status(ERROR)
-      .json({ error: `Falta el identificador de lo que busca` });
-  if (!Number.isInteger(+idState))
-    return response.status(ERROR).json({
-      error: `Necesitamos saber el identificador unico para poder buscarlo`,
-    });
-  if (!name)
-    return response
-      .status(ERROR)
-      .json({ error: `Por favor ingrese un nombre` });
   next();
 };
 
@@ -46,4 +34,4 @@ const validateName = (request, response, next) => {
   next();
 };
 
-module.exports = { validateData, validateId, validateName, validateDataPut };
+module.exports = { validateId, validateName };
