9fbf97e85e3f353dac3e064cded36f5d6c06db23
migayalach <ayalachavezmiguel@gmail.com>
2023-11-14 21:35:52 -0400

    middlewares implementados para el manejo de datos

---
+++ server/src/Middleware/stateMiddleware.js
@@ -0,0 +1,34 @@
+const ERROR = 400;
+
+const validateStateData = (request, response, next) => {
+  const { name } = request.body;
+  const minLength = 4;
+  if (!name)
+    return response
+      .status(ERROR)
+      .json({ error: `Por favor ingrese un nombre` });
+  if (name.length < minLength)
+    return response
+      .status(ERROR)
+      .json({ error: `El nombre es muy corto para poder ser valido` });
+  next();
+};
+
+const validateStateDataPut = (request, response, next) => {
+  const { idState, name } = request.body;
+  if (!idState)
+    return response
+      .status(ERROR)
+      .json({ error: `Falta el identificador de lo que busca` });
+  if (!Number.isInteger(+idState))
+    return response.status(ERROR).json({
+      error: `Necesitamos saber el identificador unico para poder buscarlo`,
+    });
+  if (!name)
+    return response
+      .status(ERROR)
+      .json({ error: `Por favor ingrese un nombre` });
+  next();
+};
+
+module.exports = { validateStateData, validateStateDataPut };
