3e3f9c1cc3d202319deab4c291bc1c82dedcfc0f
migayalach <ayalachavezmiguel@gmail.com>
2023-11-15 12:26:09 -0400

    implementacion de try catch

--- server/src/Middleware/formMiddleware.js
+++ server/src/Middleware/formMiddleware.js
@@ -2,65 +2,42 @@ const ERROR = 400;
 
 const validateFormData = (request, response, next) => {
   const { idUser, idState, idProcedure, equipment, credential } = request.body;
-  if (!idUser)
-    return response.status(ERROR).json({ error: `Se necesita un usuario` });
-  if (!Number.isInteger(+idUser))
-    return response.status(ERROR).json({ error: `Revise el usuario existe` });
-
-  if (!idState)
-    return response.status(ERROR).json({ error: `Se necesita un estado` });
-  if (!Number.isInteger(+idState))
-    return response.status(ERROR).json({ error: `Revise el estado existe` });
-
-  if (!idProcedure)
-    return response.status(ERROR).json({ error: `Se necesita un proceso` });
-  if (!Number.isInteger(+idProcedure))
-    return response.status(ERROR).json({ error: `Revise el proceso existe` });
-
-  if (!equipment.length)
-    return response
-      .status(ERROR)
-      .json({ error: `Se necesita por lo menos un equipo` });
-
-  if (!credential.length)
-    return response
-      .status(ERROR)
-      .json({ error: `Se necesita por lo menos una credencial` });
-
-  next();
+  try {
+    if (!idUser) throw Error(`Se necesita un usuario`);
+    if (!Number.isInteger(+idUser)) throw Error(`Revise el usuario existe`);
+    if (!idState) throw Error(`Se necesita un estado`);
+    if (!Number.isInteger(+idState)) throw Error(`Revise el estado existe`);
+    if (!idProcedure) throw Error(`Se necesita un proceso`);
+    if (!Number.isInteger(+idProcedure))
+      throw Error(`Revise el proceso existe`);
+    if (!equipment.length) throw Error(`Se necesita por lo menos un equipo`);
+    if (!credential.length)
+      throw Error(`Se necesita por lo menos una credencial`);
+    return next();
+  } catch (error) {
+    return response.status(ERROR).json({ form: false, error: error.message });
+  }
 };
 
 const validateFormDataPut = (request, response, next) => {
   const { idForm, idUser, idState, idProcedure, credential, equipment } =
     request.body;
-    
-  if (!Number.isInteger(+idForm))
-    return response.status(ERROR).json({ error: `No se encontro lo busca` });
-  if (!idUser)
-    return response.status(ERROR).json({ error: `Se necesita un usuario` });
-  if (!Number.isInteger(+idUser))
-    return response.status(ERROR).json({ error: `Revise el usuario existe` });
-
-  if (!idState)
-    return response.status(ERROR).json({ error: `Se necesita un estado` });
-  if (!Number.isInteger(+idState))
-    return response.status(ERROR).json({ error: `Revise el estado existe` });
-
-  if (!idProcedure)
-    return response.status(ERROR).json({ error: `Se necesita un proceso` });
-  if (!Number.isInteger(+idProcedure))
-    return response.status(ERROR).json({ error: `Revise el proceso existe` });
-
-  if (!equipment.length)
-    return response
-      .status(ERROR)
-      .json({ error: `Se necesita por lo menos un equipo` });
-
-  if (!credential.length)
-    return response
-      .status(ERROR)
-      .json({ error: `Se necesita por lo menos una credencial` });
-  next();
+  try {
+    if (!Number.isInteger(+idForm)) throw Error(`No se encontro lo busca`);
+    if (!idUser) throw Error(`Se necesita un usuario`);
+    if (!Number.isInteger(+idUser)) throw Error(`Revise el usuario existe`);
+    if (!idState) throw Error(`Se necesita un estado`);
+    if (!Number.isInteger(+idState)) throw Error(`Revise el estado existe`);
+    if (!idProcedure) throw Error(`Se necesita un proceso`);
+    if (!Number.isInteger(+idProcedure))
+      throw Error(`Revise el proceso existe`);
+    if (!equipment.length) throw Error(`Se necesita por lo menos un equipo`);
+    if (!credential.length)
+      throw Error(`Se necesita por lo menos una credencial`);
+    return next();
+  } catch (error) {
+    return response.status(ERROR).json({ form: false, error: error.message });
+  }
 };
 
 module.exports = { validateFormData, validateFormDataPut };
