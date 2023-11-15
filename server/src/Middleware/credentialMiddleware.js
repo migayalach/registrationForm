0b5d3f8c209527b83cb091ccd59b21c244925146
migayalach <ayalachavezmiguel@gmail.com>
2023-11-15 12:25:39 -0400

    implementacion de try catch

--- server/src/Middleware/credentialMiddleware.js
+++ server/src/Middleware/credentialMiddleware.js
@@ -2,40 +2,36 @@ const ERROR = 400;
 
 const validateCredentialData = (request, response, next) => {
   const { name, UserIdUser } = request.body;
-  console.log(UserIdUser);
   const minLength = 4;
-  if (!name)
+  try {
+    if (!name) throw Error(`Por favor ingrese un nombre`);
+    if (name.length < minLength)
+      throw Error(`El nombre es muy corto para poder ser valido`);
+    if (!UserIdUser) throw Error(`Se necesita un usuario`);
+    if (!Number.isInteger(+UserIdUser)) throw Error(`Revise el usuario existe`);
+    return next();
+  } catch (error) {
     return response
       .status(ERROR)
-      .json({ error: `Por favor ingrese un nombre` });
-  if (name.length < minLength)
-    return response
-      .status(ERROR)
-      .json({ error: `El nombre es muy corto para poder ser valido` });
-  if (!UserIdUser)
-    return response.status(ERROR).json({ error: `Se necesita un usuario` });
-  if (!Number.isInteger(+UserIdUser))
-    return response
-      .status(ERROR)
-      .json({ error: `Revise el usuario existe` });
-  next();
+      .json({ credential: false, error: error.message });
+  }
 };
 
 const validateCredentialDataPut = (request, response, next) => {
   const { idCredential, name } = request.body;
-  if (!idCredential)
+  try {
+    if (!idCredential) throw Error(`Falta el identificador de lo que busca`);
+    if (!Number.isInteger(+idCredential))
+      throw Error(
+        `Necesitamos saber el identificador unico para poder buscarlo`
+      );
+    if (!name) throw Error(`Por favor ingrese un nombre`);
+    return next();
+  } catch (error) {
     return response
       .status(ERROR)
-      .json({ error: `Falta el identificador de lo que busca` });
-  if (!Number.isInteger(+idCredential))
-    return response.status(ERROR).json({
-      error: `Necesitamos saber el identificador unico para poder buscarlo`,
-    });
-  if (!name)
-    return response
-      .status(ERROR)
-      .json({ error: `Por favor ingrese un nombre` });
-  next();
+      .json({ credential: false, error: error.message });
+  }
 };
 
 module.exports = { validateCredentialData, validateCredentialDataPut };
