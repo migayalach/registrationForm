6499be1db728f03129b0ab5e795f9be761fe2cb0
migayalach <ayalachavezmiguel@gmail.com>
2023-11-16 23:40:13 -0400

    se agrego un nuevo if que pide si o si los dos datos de entrada email y password

--- server/src/Middleware/loginMiddleware.js
+++ server/src/Middleware/loginMiddleware.js
@@ -2,13 +2,14 @@ const ERROR = 400;
 const validateLogin = (request, response, next) => {
   const { emailUser, password } = request.body;
   try {
+    if (!emailUser && !password) throw Error(`Introduzca datos por favor`);
     if (!emailUser) throw Error(`Falta email`);
     if (!password) throw Error(`Flata la contraseña`);
     return next();
   } catch (error) {
     return response
       .status(ERROR)
-      .json({ loginForm: false, error: error.message });
+      .json({ access: false, message: error.message });
   }
 };
 
