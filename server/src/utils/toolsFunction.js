a1ed594fdf41fdca67d7c0c77dabe20424d00eb4
migayalach <ayalachavezmiguel@gmail.com>
2023-11-18 17:55:21 -0400

    se agrego un parametro mas para la entrada de datos el cual es la key del objeto

--- server/src/utils/toolsFunction.js
+++ server/src/utils/toolsFunction.js
@@ -1,12 +1,12 @@
-const orderFuc = (array, order) => {
+const orderFuc = (array, order, key) => {
   let n = array.length,
     aux = 0,
     temp = 0;
   for (let i = 0; i < n - 1; i++) {
     aux = i;
     for (let j = i + 1; j < n; j++) {
-      let valor1 = valor(array[j].nameUser);
-      let valor2 = valor(array[aux].nameUser);
+      let valor1 = valor(array[j][key]);
+      let valor2 = valor(array[aux][key]);
       if (valor1 < valor2 && order === "ASC") aux = j;
       if (valor1 > valor2 && order === "DESC") aux = j;
     }
