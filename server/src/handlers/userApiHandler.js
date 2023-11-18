5a0a69841d68a3bf14accaa6dd776caa32dbf8bf
migayalach <ayalachavezmiguel@gmail.com>
2023-11-18 17:57:26 -0400

    se actualizo getUserApiName esperando un parametro de entrda el cual es order

--- server/src/handlers/userApiHandler.js
+++ server/src/handlers/userApiHandler.js
@@ -1,99 +1,18 @@
-// const SUCCESS = 200;
-// const ERROR = 400;
-
-// const {
-//   createUserApi,
-//   getUserApiDataId,
-//   getAllUserApi,
-//   getNameUserApi,
-//   updateUserApi,
-//   deleteDataUserApi,
-// } = require("../controllers/userApiController");
-
-// const postUserApi = async (request, response) => {
-//   const { name, nroIdentification, email, phone, charge } = request.body;
-//   try {
-//     const userApiCreate = await createUserApi(
-//       name,
-//       +nroIdentification,
-//       email,
-//       phone,
-//       charge
-//     );
-//     response.status(SUCCESS).json({ create: true, userApiCreate });
-//   } catch (error) {
-//     response.status(ERROR).json({ error: error.message });
-//   }
-// };
-
-// const getUserApiId = async (request, response) => {
-//   const { idUserApi } = request.params;
-//   try {
-//     const getUser = await getUserApiDataId(idUserApi);
-//     response.status(SUCCESS).json(getUser);
-//   } catch (error) {
-//     response.status(ERROR).json({ error: error.message });
-//   }
-// };
-
-// const getUserApiName = async (request, response) => {
-//   const { name } = request.query;
-//   try {
-//     const resController = name ? await getNameUserApi(name) : await getAllUserApi();
-//     response.status(SUCCESS).json(resController);
-//   } catch (error) {
-//     response.status(ERROR).json({ error: error.message });
-//   }
-// };
-
-// const putUserApi = async (request, response) => {
-//   const { idUserApi, name, nroIdentification, email, phone, charge } =
-//     request.body;
-//   try {
-//     const dataResponse = await updateUserApi(
-//       idUserApi,
-//       name,
-//       nroIdentification,
-//       email,
-//       phone,
-//       charge
-//     );
-//     response.status(SUCCESS).json({
-//       update: true,
-//       dataResponse,
-//     });
-//   } catch (error) {
-//     response.status(ERROR).json({ error: error.message });
-//   }
-// };
-
-// const deleteUserApi = async (request, response) => {
-//   const { idUserApi } = request.params;
-//   try {
-//     const deleteUser = await deleteDataUserApi(idUserApi);
-//     response.status(SUCCESS).json(deleteUser);
-//   } catch (error) {
-//     response.status(ERROR).json({ error: error.message });
-//   }
-// };
-
-// module.exports = {
-//   postUserApi,
-//   getUserApiId,
-//   getUserApiName,
-//   putUserApi,
-//   deleteUserApi,
-// };
-
-// 5 de octubre - nuevo
 const SUCCESS = 200;
 const ERROR = 400;
 
-const { getAllUserApi, getUserApiDataId  } = require("../controllers/userApiController");
+const {
+  getAllUserApi,
+  getUserApiDataId,
+  orderDataUserApi,
+} = require("../controllers/userApiController");
 
 const getUserApiName = async (request, response) => {
+  const { order } = request.query;
   try {
-    const dataResponse = await getAllUserApi();
+    const dataResponse = order
+      ? await orderDataUserApi(order)
+      : await getAllUserApi();
     response.status(SUCCESS).json(dataResponse);
   } catch (error) {
     response.status(ERROR).json({ error: error.message });
@@ -113,5 +32,4 @@ const getUserApiId = async (request, response) => {
 module.exports = {
   getUserApiName,
   getUserApiId,
-  
 };
