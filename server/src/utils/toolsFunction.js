bff3516151d041191f4bfe3e5e3e07a071357f9c
migayalach <ayalachavezmiguel@gmail.com>
2023-11-18 13:55:25 -0400

    se creo una funcion para poder ordernar en orden alfabetico ASC Y DESC

---
+++ server/src/utils/toolsFunction.js
@@ -0,0 +1,23 @@
+module.exports = orderFuc = (array, order) => {
+  let n = array.length,
+    aux = 0,
+    temp = 0;
+  for (let i = 0; i < n - 1; i++) {
+    aux = i;
+    for (let j = i + 1; j < n; j++) {
+      let valor1 = valor(array[j].nameUser);
+      let valor2 = valor(array[aux].nameUser);
+      if (valor1 < valor2 && order === "ASC") aux = j;
+      if (valor1 > valor2 && order === "DESC") aux = j;
+    }
+    temp = array[aux];
+    array[aux] = array[i];
+    array[i] = temp;
+  }
+  return array;
+};
+
+const valor = (text) => {
+  let character = text.split("")[0];
+  return character.charCodeAt();
+};
